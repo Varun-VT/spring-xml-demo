9560de3e3516352042a669e30c66fa89a678c634
Varun-VT <trip_varun@outlook.com>
2019-10-01 11:34:49 +0530

    Task2Done

--- src/main/java/com/stackroute/Actor.java
+++ src/main/java/com/stackroute/Actor.java
@@ -10,29 +10,29 @@ public class Actor {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getGender() {
+//        return gender;
+//    }
+//
+//    public void setGender(String gender) {
+//        this.gender = gender;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
 
     @Override
     public String toString() {
--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,7 +1,16 @@
 package com.stackroute;
 
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.beans.factory.xml.XmlReaderContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
 /**
  * Hello world!
@@ -15,8 +24,8 @@ public class Main
     Movie movie = context.getBean("MovieA", Movie.class);
     System.out.println(movie.getActor().toString());
 
-    ApplicationContext context2 = new FileSystemXmlApplicationContext("src/main/resources/beans.xml");
-    Movie movieB = context2.getBean("MovieB", Movie.class);
+        XmlBeanFactory
+
     System.out.println(movie == movieB);
     }
 }
