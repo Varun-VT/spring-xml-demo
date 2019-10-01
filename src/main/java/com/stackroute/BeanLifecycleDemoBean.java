4037345860069d3d916c2fcba44a1201afda2745
Varun-VT <trip_varun@outlook.com>
2019-10-01 18:00:29 +0530

    AllTask5PointsCovered

--- src/main/java/com/stackroute/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/BeanLifecycleDemoBean.java
@@ -22,4 +22,12 @@ public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
     public void afterPropertiesSet() throws Exception {
         System.out.println("Init method after properties are set : " + message);
     }
+
+    public void customInit(){
+        System.out.println("Hey! You're in my custom Init method");
+    }
+
+    public void customDestroy(){
+        System.out.println("Hey! You're in my custom Destroy method");
+    }
 }
